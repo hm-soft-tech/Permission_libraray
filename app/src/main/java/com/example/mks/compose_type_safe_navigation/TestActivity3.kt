a46dbc42a9c876615ffb4742b2d6c6515fdb3b7e
hp <hamzamalik96atd@gmail.com>
2024-08-26 12:28:53 +0500

    commit in activity 3

---
+++ app/src/main/java/com/example/mks/compose_type_safe_navigation/TestActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.mks.compose_type_safe_navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test3)
+    }
+}
